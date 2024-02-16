2816440785f0e842c787be72d984b96e85fd7c60
csc32 <sanzonettycar@gmail.com>
2024-02-16 15:53:18 -0400

    style: update text color, animation and hover.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ export default function Item({ name }) {
     <li>
       <a
         href="#"
-        className="block py-2 px-3 rounded hover:text-gray-100 md:hover:bg-transparent md:hovertext-gray-100 md:p-0 md:dark:hover:text-gray-200 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        className="block py-2 px-3 rounded hover:bg-white md:hover:bg-transparent md:hover:text-white md:p-0 md:dark:hover:text-gray-200 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition duration-50 ease-linear"
       >
         {name}
       </a>
