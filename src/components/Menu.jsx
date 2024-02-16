0cba7252324e18ab311151ed640c281b79971016
csc32 <sanzonettycar@gmail.com>
2024-02-16 15:37:33 -0400

    add Menu component to render Items elements

---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+import Item from './Item.jsx';
+import ITEMS from '../constants.js';
+
+export default function Menu() {
+  return (
+    <div
+      className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+      id="navbar-sticky"
+    >
+      <ul className="flex flex-col text-indigo-300 p-4 md:p-0 mt-4 font-medium border border-indigo-100 rounded-lg bg-indigo-900 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-indigo-900 dark:bg-indigo-300 md:dark:indigo-500 dark:indigo-700">
+        {ITEMS.map((el) => (
+          <Item name={el.name} />
+        ))}
+      </ul>
+    </div>
+  );
+}
