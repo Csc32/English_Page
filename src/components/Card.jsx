b5950f5f6d6d97fc17b4545f9629f5fa087fe78b
csc32 <sanzonettycar@gmail.com>
2024-02-17 11:58:27 -0400

    add new component and update style of the Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 export default function Card({ title, description, image, imageInfo }) {
   return (
-    <article class="max-w-sm bg-gray-500 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <img class="rounded-t-lg" src={image} alt={imageInfo} />
-      <div class="p-5">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    <article className="max-w-sm bg-gray-500 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <img className="rounded-t-lg" src={image} alt={imageInfo} />
+      <div className="p-5">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
         </h5>
-        <p class="mb-2 font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-2 font-normal text-gray-700 dark:text-gray-400">
           {description}
         </p>
       </div>
