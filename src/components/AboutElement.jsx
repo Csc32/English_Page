977300f652ba42315ddfd481ef394a1a1ccca654
csc32 <sanzonettycar@gmail.com>
2024-02-17 11:57:54 -0400

    style: update style to fix responsive

--- src/components/AboutElement.jsx
+++ src/components/AboutElement.jsx
@@ -1,6 +1,6 @@
 export default function AboutElement({ title, description }) {
   return (
-    <article className="w-full flex flex-col justify-center items-center text-gray-900 h-44 bg-transparent shadow-md gap-1">
+    <article className="w-full flex flex-col justify-center items-center text-gray-900 h-44 bg-transparent shadow-md gap-1 rounded-lg flex-1 p-5">
       <h3 className="text-2xl flex-1 font-bold">{title}</h3>
       <p className="text-md text-gray-700 flex-1">{description}</p>
     </article>
