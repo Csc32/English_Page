96ef61f0fdf240192acd85049b379beae80e6bf4
csc32 <sanzonettycar@gmail.com>
2024-02-17 11:57:27 -0400

    add new section to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import NavBar from './components/NavBar.jsx';
 import './App.css';
+import NavBar from './components/NavBar.jsx';
 import ImageSection from './components/ImageSection.jsx';
+import CardSection from './components/CardSection.jsx';
+import AboutSection from './components/AboutSection.jsx';
+import EducationSection from './components/EducationSection.jsx';
 
 function App() {
   return (
@@ -8,6 +11,15 @@ function App() {
       <NavBar />
       <section className="w-full">
         <ImageSection />
+      </section>
+      <section className="w-full  p-8">
+        <CardSection />
+      </section>
+      <section className="w-full ">
+        <EducationSection />
+      </section>
+      <section className="w-full p-8 flex flex-col justify-center items-center gap-10 mt-5">
+        <AboutSection />
       </section>
     </>
   );
