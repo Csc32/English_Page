750826a0fc0f97a54e633eb2246b23df25587f67
csc32 <sanzonettycar@gmail.com>
2024-02-17 20:00:15 -0400

    refactor(Button): add props to customize style

---
+++ src/components/Form.jsx
@@ -0,0 +1,40 @@
+import { INPUT_CONTENTS, BUTTON_STYLES } from '../constants.js';
+import Input from './Input.jsx';
+import Button from './Button.jsx';
+
+export default function Form() {
+  return (
+    <form className="w-full text-left text-sm z-10">
+      {INPUT_CONTENTS.map((el) => (
+        <Input
+          inputName={el.inputName}
+          type={el.type}
+          id={el.id}
+          placeholder={el.placeholder}
+          key={el.id}
+        />
+      ))}
+      <div className="w-full flex flex-col mb-5">
+        <label
+          htmlFor="message"
+          className="block mb-2 text-sm font-bold text-white flex-1"
+        >
+          Escriba su mensaje
+        </label>
+        <textarea
+          id="message"
+          rows="4"
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Tu mensaje"
+        ></textarea>
+      </div>
+      <div className="flex justify-center items-center mb-5">
+        <Button
+          name={BUTTON_STYLES[0].submit.name}
+          style={BUTTON_STYLES[0].submit.style}
+          type={BUTTON_STYLES[0].submit.type}
+        />
+      </div>
+    </form>
+  );
+}
--- src/constants.js
+++ src/constants.js
@@ -56,6 +56,38 @@ export const ABOUT_ELEMENTS = [
   {
     title: '¿Como te puedes comunicar con nosotros ?',
     description:
-      'A traves de nuestras redes sociales, puedes seguirnos por instagram, facebook o nuestro canal de telegram 😄, por esos medios responderemos todas tus dudas ✌.',
+      'Contactanos a través de nuestras redes sociales, puedes seguirnos por instagram, facebook o nuestro canal de telegram 😄. Por estos medios responderemos todas tus dudas ✌.',
+  },
+];
+
+export const INPUT_CONTENTS = [
+  {
+    inputName: 'Nombre',
+    type: 'text',
+    id: 'name',
+    placeholder: 'Jhoen',
+  },
+  {
+    inputName: 'Apellido',
+    type: 'text',
+    id: 'lastname',
+    placeholder: 'Doe',
+  },
+];
+
+export const BUTTON_STYLES = [
+  {
+    normal: {
+      style:
+        'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
+      name: 'registrarse',
+      type: 'button',
+    },
+    submit: {
+      style:
+        'text-gray-800 bg-white py-3 px-2 hover:bg-gray-600 hover:text-white transition ease-linear duration-300 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm px-4 py-2 text-center',
+      name: 'Enviar Mensaje',
+      type: 'submit',
+    },
   },
 ];
