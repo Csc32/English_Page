1530cb89d226cbfd554647c01eb11c3921c7db79
csc32 <sanzonettycar@gmail.com>
2024-02-17 20:02:01 -0400

    add key to the elements

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -4,12 +4,13 @@ import Card from './Card.jsx';
 export default function CardSection() {
   return (
     <>
-      {CARDS_ELEMENTS.map((el) => (
+      {CARDS_ELEMENTS.map((el, index) => (
         <Card
           title={el.title}
           description={el.description}
           image={el.image}
           imageInfo={el.imageInfo}
+          key={index}
         />
       ))}
     </>
