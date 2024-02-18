691c3ab0899d234d2a569c3084d7e6cd673786d2
csc32 <sanzonettycar@gmail.com>
2024-02-18 07:41:34 -0400

    style: set the same color to the inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -10,7 +10,7 @@ export default function Input({ inputName, type, id, placeholder }) {
       <input
         type={type}
         id={id}
-        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg flex-1 focus:ring-blue-500 focus:border-gray-900 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:shadow-sm-light"
+        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg flex-1 focus:ring-gray-400 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:shadow-sm-light"
         placeholder={placeholder}
         required
       />
